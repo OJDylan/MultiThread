4b55e51f5c6d6b92a7d28ed7beccd8f173c7ed1e
OJDylan <dooi2142@gmail.com>
2019-10-10 17:43:35 +0800

    Final

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,40 @@ import java.util.Scanner;
 
 class MyThread implements Runnable {
     private int start, end;
+    private static int total;
 
     //constructor to pass start and end number per thread
-    public MyThread(int s, int e) {
+    MyThread(int s, int e) {
         this.start = s;
         this.end = e;
     }
 
+    //No argument constructor to call getSum method
+    MyThread(){}
+
     @Override
     public void run() {
         //print thread start
-        System.out.println("Thread " + Thread.currentThread().getId() + ": Started");
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Started!");
         try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); } //pause
 
         int sum = 0;
-        int total = 0;
 
         //main summation
         for (int i = start; i <= end; i++) {
-            System.out.println("Thread " + Thread.currentThread().getId() + " on: " + i);
+            System.out.println("Thread " + Thread.currentThread().getId() + " is on --> " + i);
             sum = sum + i;
             try { Thread.sleep(100); } catch (InterruptedException e) { e.printStackTrace(); } //pause
-            total += sum;
         }
         System.out.println("Thread " + Thread.currentThread().getId() + " sum = " + sum); //print sum of each thread
+        total += sum;
         System.out.println(Thread.currentThread().getId() + " Exiting"); //print thread exit
     }
 
+    //method to return total sum of each thread
+    int getSum() {
+        return total;
+    }
 }
 
 public class Main {
@@ -51,17 +58,19 @@ public class Main {
 
         Thread t = new Thread();
 
-        //create threads
-        for (int i=0; i<n; i++) {
-            t = new Thread(new MyThread(start, start + sub_range - 1)); //passes start number to end number divided by the number of threads
-            t.start();
-            Thread.sleep(500);
-            start += sub_range; //starts on the number after the last end number
+        if (range % n == 0) {
+            //create threads
+            for (int i=0; i<n; i++) {
+                t = new Thread(new MyThread(start, start + sub_range - 1)); //passes start number to end number divided by the number of threads
+                t.start();
+                Thread.sleep(500);
+                start += sub_range; //starts on the number after the last end number
+            }
+            t.join(); //join threads before displaying end message
+            System.out.println("\nAll threads completed."); //end message
+            System.out.println("\nTotal sum is: " + new MyThread().getSum()); //print total sum
+        } else {
+            System.out.println("Sorry, the range of numbers is not divisible by the number of threads, please try again");
         }
-
-        t.join(); //join threads before displaying end message
-        System.out.println("\nAll threads completed."); //end message
-
-        //**display total sum here**
     }
 }
