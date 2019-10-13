ebf9ed1fc9e8f5fab30c673c1b03c9b75711acd1
OJDylan <dooi2142@gmail.com>
2019-10-13 14:49:43 +0800

    Final (minor changes)

--- src/Main.java
+++ src/Main.java
@@ -49,33 +49,37 @@ public class Main {
         System.out.print("Enter ending number: ");
         int end = new Scanner(System.in).nextInt();
 
-        //get number of threads from user
-        System.out.print("Enter the number of threads: ");
-        int numThreads = new Scanner(System.in).nextInt();
-
-        System.out.println("==============================\n");
-
-        int range = ((end - start) + 1); //calculate range
-        int sub_range = range/numThreads; //divides range into sub-ranges by the number of threads
-
-        Thread t = new Thread();
-
-        if (range % numThreads == 0) {
-            //create the threads
-            for (int i=0; i<numThreads; i++) {
-                t = new Thread(new MyThread(start, start + sub_range - 1)); //passes start and end number of a subrange
-                t.start();
-                Thread.sleep(500); //pause
-                start += sub_range; //starts on number after the last end number
-            }
-            t.join(); //join threads before displaying end message
-            System.out.println("\nAll threads completed."); //end message
-            System.out.println("\n==============================");
-            System.out.println("Total sum is: " + new MyThread().getSum()); //print total sum
-            System.out.println("==============================");
+        if (start > end) {
+            System.out.println("\nERROR: Starting number cannot be larger than the ending number, please try again");
         } else {
-            System.out.println(
-                    "Sorry, the range of numbers cannot be equally divided by the number of threads, please try again");
+            //get number of threads from user
+            System.out.print("Enter the number of threads: ");
+            int numThreads = new Scanner(System.in).nextInt();
+
+            System.out.println("==============================\n");
+
+            int range = ((end - start) + 1); //calculate range
+            int sub_range = range / numThreads; //divides range into sub-ranges by the number of threads
+
+            Thread t = new Thread();
+
+            if (range % numThreads == 0) {
+                //create the threads
+                for (int i = 0; i < numThreads; i++) {
+                    t = new Thread(new MyThread(start, start + sub_range - 1)); //passes start and end number of a subrange
+                    t.start();
+                    Thread.sleep(500);
+                    start += sub_range; //starts on number after the last end number
+                }
+                t.join(); //join threads before displaying end message
+                System.out.println("\nAll threads completed."); //end message
+                System.out.println("\n==============================");
+                System.out.println("Total sum is: " + new MyThread().getSum()); //print total sum
+                System.out.println("==============================");
+            } else {
+                System.out.println(
+                        "ERROR: The range of numbers cannot be equally divided among the number of threads, please try again");
+            }
         }
     }
 }
