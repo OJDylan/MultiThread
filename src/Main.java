b1ac59afc37a1b4a674e676e8dc9ac718d8737bf
OJDylan <dooi2142@gmail.com>
2019-10-31 13:44:45 +0800

    Final (minor changes)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/*
+    Original Author : Ooi Junjie Dylan 0120185
+    Finished Date   : 10/10/19
+
+    DISCLAIMER      :   This assignment is done through MY own self study and referencing from online sources.
+
+    Thank you :)
+ */
+
 import java.util.Scanner;
 
 class MyThread implements Runnable {
@@ -49,6 +58,7 @@ public class Main {
         System.out.print("Enter ending number: ");
         int end = new Scanner(System.in).nextInt();
 
+        //checks if start number is larger than the end number
         if (start > end) {
             System.out.println("\nERROR: Starting number cannot be larger than the ending number, please try again");
         } else {
@@ -56,29 +66,34 @@ public class Main {
             System.out.print("Enter the number of threads: ");
             int numThreads = new Scanner(System.in).nextInt();
 
-            System.out.println("==============================\n");
-
-            int range = ((end - start) + 1); //calculate range
-            int sub_range = range / numThreads; //divides range into sub-ranges by the number of threads
-
-            Thread t = new Thread();
-
-            if (range % numThreads == 0) {
-                //create the threads
-                for (int i = 0; i < numThreads; i++) {
-                    t = new Thread(new MyThread(start, start + sub_range - 1)); //passes start and end number of a subrange
-                    t.start();
-                    Thread.sleep(500);
-                    start += sub_range; //starts on number after the last end number
-                }
-                t.join(); //join threads before displaying end message
-                System.out.println("\nAll threads completed."); //end message
-                System.out.println("\n==============================");
-                System.out.println("Total sum is: " + new MyThread().getSum()); //print total sum
-                System.out.println("==============================");
+            //checks if threads is 0
+            if (numThreads == 0) {
+                System.out.println("\nERROR: Number of threads cannot be 0");
             } else {
-                System.out.println(
-                        "ERROR: The range of numbers cannot be equally divided among the number of threads, please try again");
+                System.out.println("==============================\n");
+
+                int range = ((end - start) + 1); //calculate range
+                int sub_range = range / numThreads; //divides range into sub-ranges by the number of threads
+
+                Thread t = new Thread();
+
+                if (range % numThreads == 0) {
+                    //create the threads
+                    for (int i = 0; i < numThreads; i++) {
+                        t = new Thread(new MyThread(start, start + sub_range - 1)); //passes start and end number of a subrange
+                        t.start();
+                        Thread.sleep(500);
+                        start += sub_range; //starts on number after the last end number
+                    }
+                    t.join(); //join threads before displaying end message
+                    System.out.println("\nAll threads completed."); //end message
+                    System.out.println("\n==============================");
+                    System.out.println("Total sum is: " + new MyThread().getSum()); //print total sum
+                    System.out.println("==============================");
+                } else {
+                    System.out.println(
+                            "ERROR: The range of numbers cannot be equally divided among the number of threads, please try again");
+                }
             }
         }
     }
